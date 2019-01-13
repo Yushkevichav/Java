ea641d4481c08d6793f120798c0597ca84769a35
Andrew Yushkevich <yushkevichandrew@gmail.com>
2019-01-13 17:58:43 +0300

    Исправления

--- src/yushkevich/lesson_1/homework/Part2.java
+++ src/yushkevich/lesson_1/homework/Part2.java
@@ -3,12 +3,12 @@ package yushkevich.lesson_1.homework;
 
 public class Part2 {
     public static void main(String[] args) {
-        checkOne(1, 2, 3, 4);                      //вызвал метод checkOne
+        checkOne(1, 2, 3, 4);
     }
 
-    private static int checkOne(int a, int b, int c, int d) {      //создал метод checkOne
-        int result = a * (b + (c / d));  //инициализировал переменную result
-        System.out.println(result);      //вывел на консоль переменную result
-        return (result);                 //вернул в main результат вычисления result
+    private static int checkOne(int a, int b, int c, int d) {
+        int result = a * (b + (c / d));
+        System.out.println(result);
+        return (result);
     }
 }
