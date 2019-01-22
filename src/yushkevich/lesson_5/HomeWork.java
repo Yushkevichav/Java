842467d2a2431f312b7d3a8334cd4cd7de801daf
Andrew Yushkevich <yushkevichandrew@gmail.com>
2019-01-22 20:35:01 +0300

    Решение

--- src/yushkevich/lesson_5/HomeWork.java
+++ src/yushkevich/lesson_5/HomeWork.java
@@ -1,7 +1,36 @@
+/**
+ * Java 1. Lesson 5
+ *
+ * @author Andrey Yushkevich
+ * version dated Jan 22, 2019
+ **/
+
 package yushkevich.lesson_5;
 
 public class HomeWork {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        new HomeWork().description();
+    }
+
+    void description() {
+        Worker workersArr[] = new Worker[5];
+        workersArr[0] = new Worker("Test1", "Test1", "Test1", "89991112231", 10500.50, 23);
+        workersArr[1] = new Worker("Test2", "Test2", "Test2", "89991112232", 20500.50, 33);
+        workersArr[2] = new Worker("Test3", "Test3", "Test3", "89991112233", 30500.50, 43);
+        workersArr[3] = new Worker("Test4", "Test4", "Test4", "89991112234", 40500.50, 53);
+        workersArr[4] = new Worker("Test5", "Test5", "Test5", "89991112235", 50500.50, 63);
+
+        for (int i = 0; i < workersArr.length; i++) {
+            if (workersArr[i].age > 40) {
+                System.out.println("ФИО: " + workersArr[i].name + ", "
+                        + "Должность: " + workersArr[i].position + ", "
+                        + "Почта: " + workersArr[i].email + ", "
+                        + "Телефон: " + workersArr[i].phone + ", "
+                        + "ЗП: " + workersArr[i].salary + ", "
+                        + "Возраст: " + workersArr[i].age);
+            }
+        }
     }
 }
+
--- src/yushkevich/lesson_5/Worker.java
+++ src/yushkevich/lesson_5/Worker.java
@@ -1,19 +1,21 @@
+/**
+ * Java 1. Lesson 5
+ *
+ * @author Andrey Yushkevich
+ * version dated Jan 22, 2019
+ **/
+
 package yushkevich.lesson_5;
 
 public class Worker {
-    private String name;
-    private String position;
-    private String email;
-    private String phone;
-    private double salary;
-    private int age;
+    protected String name;
+    protected String position;
+    protected String email;
+    protected String phone;
+    protected double salary;
+    protected int age;
 
-
-    public static void main(String[] args) {
-        description();
-    }
-
-    private Worker(String name, String position, String email, String phone, double salary, int age) {
+    protected Worker(String name, String position, String email, String phone, double salary, int age) {
         this.name = name;
         this.position = position;
         this.email = email;
@@ -21,23 +23,4 @@ public class Worker {
         this.salary = salary;
         this.age = age;
     }
-
-    private static void description() {
-        Worker workersArr[] = new Worker[5];
-        workersArr[0] = new Worker("Test1", "Test1", "Test1", "89991112231", 10500.50, 23);
-        workersArr[1] = new Worker("Test2", "Test2", "Test2", "89991112232", 20500.50, 33);
-        workersArr[2] = new Worker("Test3", "Test3", "Test3", "89991112233", 30500.50, 43);
-        workersArr[3] = new Worker("Test4", "Test4", "Test4", "89991112234", 40500.50, 53);
-        workersArr[4] = new Worker("Test5", "Test5", "Test5", "89991112235", 50500.50, 63);
-
-        for (int i = 0; i < workersArr.length; i++) {
-            if (workersArr[i].age > 40) {
-                System.out.println(workersArr[i].name + " " + workersArr[i].position + " " + workersArr[i].email + " " + workersArr[i].phone + " " + workersArr[i].salary + " " + workersArr[i].age);
-            }else {
-                System.out.println("Резултаты отсутствуют");
-            }
-        }
-    }
 }
-
-
