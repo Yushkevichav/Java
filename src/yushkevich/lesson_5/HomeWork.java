20c60b53b60c8319cfba48f709f38081ee076933
Andrew Yushkevich <yushkevichandrew@gmail.com>
2019-01-22 10:31:44 +0300

    Test object

--- src/yushkevich/lesson_5/HomeWork.java
+++ src/yushkevich/lesson_5/HomeWork.java
@@ -3,6 +3,9 @@ package yushkevich.lesson_5;
 public class HomeWork {
 
     public static void main(String[] args) {
-        Worker one = new Worker();
+        Worker worker1 = new Worker("Test1", "test1", "test1", "test1", 40000.5, 25);
+        Worker worker2 = new Worker("Test2", "test2", "test2", "test2", 40000.5, 63);
+        Worker worker3 = new Worker("Test3", "test3", "test3", "test3", 40000.5, 41);
+        Worker worker4 = new Worker("Test4", "test4", "test4", "test4", 40000.5, 36);
     }
 }
--- src/yushkevich/lesson_5/Worker.java
+++ src/yushkevich/lesson_5/Worker.java
@@ -4,64 +4,16 @@ public class Worker {
     private String name;
     private String position;
     private String email;
-    private int phone;
-    private int salary;
+    private String phone;
+    private double salary;
     private int age;
 
-    public void setName(String name) {
+    public Worker(String name, String position, String email, String phone, double salary, int age) {
         this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setPosition(String position) {
         this.position = position;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
-    public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setPhone(int phone) {
         this.phone = phone;
+        this.salary = salary;
+        this.age = age;
     }
-
-    public int getPhone() {
-        return phone;
-    }
-
-    public void setSalary(int salary) {
-        if (salary >= 0) {
-            this.salary = salary;
-        } else {
-            System.out.println("Введено некорректное значение зп");
-        }
-    }
-
-    public int getSalary() {
-        return salary;
-    }
-
-    public void setAge(int age) {
-        if (age >= 0) {
-            this.age = age;
-        } else {
-            System.out.println("Введен некорректный возраст");
-        }
-    }
-
-    public int getAge() {
-        return age;
-    }
-
 }
