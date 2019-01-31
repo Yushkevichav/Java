50ba35af335a25438678c204d854cf27bf9f0139
Andrew Yushkevich <yushkevichandrew@gmail.com>
2019-01-31 18:12:17 +0300

    Исправления

--- src/yushkevich/lesson_6/Cat.java
+++ src/yushkevich/lesson_6/Cat.java
@@ -9,37 +9,37 @@ package yushkevich.lesson_6;
 
 public class Cat extends Animal {
 
-    protected final int maxRun = 200;                                       //неизменяемая переменная = максимальная длина забега
-    protected int factRun;                                                  //фактическая длина бега
-    protected final boolean maxSwim = true;                                 //неизменяемая булева переменная, т.к. наш кот не умеет плавать - выболняет роль чекера заплыва
-    protected boolean factSwim;                                             //фактическая длина заплыва
-    protected final int maxJump = 2;                                        //неизменяемая переменная = максимальная высота прыжка
-    protected int factJump;                                                 //фактическая высота прыжка
+    protected final int maxRun = 200;                                           //неизменяемая переменная = максимальная длина забега
+    protected int factRun;                                                      //фактическая длина бега
+    protected final boolean maxSwim = true;                                     //неизменяемая булева переменная, т.к. наш кот не умеет плавать - выболняет роль чекера заплыва
+    protected boolean factSwim;                                                 //фактическая длина заплыва
+    protected final int maxJump = 2;                                            //неизменяемая переменная = максимальная высота прыжка
+    protected int factJump;                                                     //фактическая высота прыжка
 
     @Override
-    public void run() {                                                     //переопределенный метод, проверяет бег
-        if (factRun > maxRun) {                                             //если фактическая длина бега > максимальной
+    public void run() {                                                         //переопределенный метод, проверяет бег
+        if (factRun > maxRun) {                                                 //если фактическая длина бега > максимальной
             System.out.println("Коты не могут так бегать");
-        } else {                                                            //в остальных случаях
+        } else {                                                                //в остальных случаях
             System.out.println("Кот пробежал(м): " + factRun);
         }
     }
 
     @Override
-    public void swim() {                                                    //переопределенный метод, проверяет плавание
-        if (factSwim == maxSwim) {                                          //если фактический результат true (
+    public void swim() {                                                        //переопределенный метод, проверяет плавание
+        if (factSwim == maxSwim) {                                              //если фактический результат true (
             System.out.println("Этот кот не умеет плавать");
-        } else {                                                            //если кот все же поплыл
-            System.out.println("Очень странный кот");
+        } else {                                                                //если кот все же поплыл
+            System.out.println("Очень странный кот. Говорил не умеет плавать");
         }
     }
 
     @Override
-    public void jump() {                                                    //переопределенный метод, проверяет прыжок
+    public void jump() {                                                        //переопределенный метод, проверяет прыжок
         if (factJump > maxJump) {
-            System.out.println("Коты не умеют так прыгать");                //если фактическая высота прыжка > максимальной
+            System.out.println("Коты не умеют так прыгать");                    //если фактическая высота прыжка > максимальной
         } else {
-            System.out.println("Кот подпрыгнул на высоту(м):" + factJump);  //в остальных случаях
+            System.out.println("Кот подпрыгнул на высоту(м):" + factJump);      //в остальных случаях
         }
     }
 }
