1fc8bd31328cd66b4b8c5488b537dccfae52be8f
Andrew Yushkevich <yushkevichandrew@gmail.com>
2019-02-04 13:27:06 +0300

    Исправления

--- src/yushkevich/lesson_7/Main.java
+++ src/yushkevich/lesson_7/Main.java
@@ -10,12 +10,12 @@ package yushkevich.lesson_7;
 class Main {
     public static void main(String[] args) {
         Plate plate = new Plate(10);
-        Cat catArr[] = new Cat[1];
+        Cat catArr[] = new Cat[5];
         catArr[0] = new Cat("Антоний");
-//        catArr[1] = new Cat("Патрокл");
-//        catArr[2] = new Cat("Поликарп");
-//        catArr[3] = new Cat("Афиноген");
-//        catArr[4] = new Cat("Аврелий");
+        catArr[1] = new Cat("Патрокл");
+        catArr[2] = new Cat("Поликарп");
+        catArr[3] = new Cat("Афиноген");
+        catArr[4] = new Cat("Аврелий");
         for (int i = 0; i < catArr.length; i++) {
             System.out.println("Еды в тарелке: " + plate);
             catArr[i].eat(plate);
