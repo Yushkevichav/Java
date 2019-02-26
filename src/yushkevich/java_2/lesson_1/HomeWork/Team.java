5db907dd43ea4b55a96cbcd3babeebdfcdd01c33
Andrew Yushkevich <yushkevichandrew@gmail.com>
2019-02-26 22:47:36 +0300

    refactor

--- src/yushkevich/java_2/lesson_1/HomeWork/Team.java
+++ src/yushkevich/java_2/lesson_1/HomeWork/Team.java
@@ -3,18 +3,21 @@ package yushkevich.java_2.lesson_1.HomeWork;
 public class Team {
 
     protected String name;
+    Human[] countTeam;
 
-    public Team(String name) {
+    public Team(String name, Human[] countTeam) {
         this.name = name;
-        Human[] countTeam = {
+        this.countTeam = new Human[]{
                 new Human(1, 2, 3, "Aa"),
                 new Human(2, 3, 4, "Bb"),
                 new Human(3, 4, 5, "Cc"),
                 new Human(4, 5, 6, "Dd"),
         };
+        this.countTeam = countTeam;
     }
 
     public void teamInfo() {
         System.out.println("Имя команды: " + name);
+        System.out.println(countTeam[0].name);
     }
 }
