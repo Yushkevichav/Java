14626890baf55392abea26fea484f8d17301e56c
Andrew Yushkevich <yushkevichandrew@gmail.com>
2019-02-27 21:17:58 +0300

    something

--- src/yushkevich/java_2/lesson_1/Course.java
+++ src/yushkevich/java_2/lesson_1/Course.java
@@ -1,10 +1,13 @@
 package yushkevich.java_2.lesson_1;
 
 public class Course {
-    public Course(){
-        Obstacle[] obstacles = {new Road(8), new Water(3), new Wall(3)};
+
+    Obstacle obstacles[];
+
+    public Course() {
+        obstacles = new Obstacle[]{new Road(8), new Water(3), new Wall(3)};
     }
 
-    public void doIt(Team s){
+    public void doIt(Team s) {
     }
 }
