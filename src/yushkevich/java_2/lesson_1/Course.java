3303af233f541e594c5cfac48f14935bd7aa186c
Andrew Yushkevich <yushkevichandrew@gmail.com>
2019-02-27 16:09:52 +0300

    something

--- src/yushkevich/java_2/lesson_1/Course.java
+++ src/yushkevich/java_2/lesson_1/Course.java
@@ -1,4 +1,10 @@
 package yushkevich.java_2.lesson_1;
 
 public class Course {
+    public Course(){
+        Obstacle[] obstacles = {new Road(8), new Water(3), new Wall(3)};
+    }
+
+    public void doIt(Team t){
+    }
 }
--- src/yushkevich/java_2/lesson_1/Team.java
+++ src/yushkevich/java_2/lesson_1/Team.java
@@ -2,20 +2,27 @@ package yushkevich.java_2.lesson_1;
 
 public class Team {
     protected String name;
+
+    Animal[] animals;
+
     public Team(String name){
         this.name = name;
-        Animal[] animals = {
+        animals = new Animal[]{
                 new Cat(5, 3),
                 new Dog(5, 10, 2),
-                new Duck(3, 10)
+                new Duck(3, 10),
+                new Duck(4, 8)
         };
     }
 
     public void showResults(){
-
+        System.out.println();
     }
 
     public void showResultsTeam(){
-
+        System.out.println("Первый участник: " + animals[0].name);
+        System.out.println("Первый участник: " + animals[1].name);
+        System.out.println("Первый участник: " + animals[2].name);
+        System.out.println("Первый участник: " + animals[3].name);
     }
 }
