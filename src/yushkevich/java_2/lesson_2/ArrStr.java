424d55288ac059d839346384619fae83ebf8d521
Andrew Yushkevich <yushkevichandrew@gmail.com>
2019-03-02 13:43:31 +0300

    something

---
+++ src/yushkevich/java_2/lesson_2/ArrStr.java
@@ -0,0 +1,10 @@
+package yushkevich.java_2.lesson_2;
+
+public class ArrStr {
+
+    ArrStr[][] arr;
+
+    public ArrStr(ArrStr[][] arr){
+        this.arr = arr;
+    }
+}
--- src/yushkevich/java_2/lesson_2/Main.java
+++ src/yushkevich/java_2/lesson_2/Main.java
@@ -1,4 +1,7 @@
 package yushkevich.java_2.lesson_2;
 
 public class Main {
+    public static void main(String[] args) {
+        ArrStr arr = new ArrStr(new ArrStr[][]{{}});
+    }
 }
