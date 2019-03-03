e3ef07f6aa00dbf580b2721a37c32689dd645e7f
Andrew Yushkevich <yushkevichandrew@gmail.com>
2019-03-03 13:15:01 +0300

    something

--- src/yushkevich/java_2/lesson_2/Main.java
+++ src/yushkevich/java_2/lesson_2/Main.java
@@ -2,6 +2,7 @@ package yushkevich.java_2.lesson_2;
 
 public class Main {
     public static void main(String[] args) {
-        Сalculations c = new Сalculations();
+        StrArr strArr = new StrArr(new String[][]{{"sdsd", "dgf"},{"sdfsdf", "rtbrb"}});
+        strArr.makeInt(strArr);
     }
 }
---
+++ src/yushkevich/java_2/lesson_2/StrArr.java
@@ -0,0 +1,19 @@
+package yushkevich.java_2.lesson_2;
+
+public class StrArr {
+
+    public String[][] ar;
+
+    public StrArr(String ar[][]) {
+        this.ar = ar;
+        this.ar = new String[2][2];
+    }
+
+    public void makeInt(StrArr s) {
+        for (String[] i : ar) {
+            for (String[] g : ar) {
+                System.out.println(ar);
+            }
+        }
+    }
+}
