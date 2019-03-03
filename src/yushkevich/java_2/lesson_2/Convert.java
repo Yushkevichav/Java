ffe90c44012150cd21dcce8b70720beda12860ae
Andrew Yushkevich <yushkevichandrew@gmail.com>
2019-03-03 18:27:28 +0300

    still bad descision

--- src/yushkevich/java_2/lesson_2/Convert.java
+++ src/yushkevich/java_2/lesson_2/Convert.java
@@ -8,21 +8,24 @@
 package yushkevich.java_2.lesson_2;
 
 public class Convert {
-    public void makeInt(StrArr s) {
-        int size_x = 4;
-        int size_y = 4;
+
+    protected int size_x = 4;
+    protected int size_y = 4;
+
+    public void makeInt(String[][] arr) {
+
         int sum = 0;
         try {
-            for (int i = 0; i < s.ar.length; i++) {
-                if (s.ar.length != size_x) {
-                    throw new MyArraySizeException("StrArr must be 4x4");
+            for (int i = 0; i < arr.length; i++) {
+                if (arr.length != size_x) {
+                    throw new MyArraySizeException("StrArr must be " + size_x + "x" + size_y);
                 }
-                for (int g = 0; g < s.ar.length; g++) {
-                    if (s.ar.length != size_y) {
-                        throw new MyArraySizeException("StrArr must be 4x4");
+                for (int g = 0; g < arr.length; g++) {
+                    if (arr.length != size_y) {
+                        throw new MyArraySizeException("StrArr must be " + size_x + "x" + size_y);
                     }
                     try {
-                        sum += Integer.parseInt(s.ar[i][g]);
+                        sum += Integer.parseInt(arr[i][g]);
                     } catch (NumberFormatException e) {
                         throw new MyArrayDataException(i, g);
                     }
--- src/yushkevich/java_2/lesson_2/Main.java
+++ src/yushkevich/java_2/lesson_2/Main.java
@@ -10,15 +10,15 @@ package yushkevich.java_2.lesson_2;
 public class Main {
     public static void main(String[] args) {
         try {
-            StrArr correct_arr = new StrArr(new String[][]{
-                    {"1", "2", "4", "9"},
-                    {"6", "4", "5", "7"},
-                    {"4", "4", "5", "7"},
-                    {"5", "4", "3", "9"}
-            });
-            new Convert().makeInt(correct_arr);
-        }catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("StrArr must be 4x4");
+            String arr[][] = new String[][]{
+                    {"1", "2", "3", "4"},
+                    {"1", "2", "3", "4"},
+                    {"1", "2", "3", "4"},
+                    {"1", "2", "3", "4"},
+            };
+            new Convert().makeInt(arr);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("StrArr must be " + new Convert().size_x + "x" + new Convert().size_y);
         }
     }
 }
