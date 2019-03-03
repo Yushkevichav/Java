49e8ac9cdc3ef6964bee47821dd737678d2f13dc
Andrew Yushkevich <yushkevichandrew@gmail.com>
2019-03-03 16:36:23 +0300

    something

--- src/yushkevich/java_2/lesson_2/MyArrayDataException.java
+++ src/yushkevich/java_2/lesson_2/MyArrayDataException.java
@@ -1,3 +1,10 @@
+/**
+ * Java 2. Lesson 2
+ *
+ * @author Andrey Yushkevich
+ * version dated March 03, 2019
+ */
+
 package yushkevich.java_2.lesson_2;
 
 public class MyArrayDataException extends Exception {
