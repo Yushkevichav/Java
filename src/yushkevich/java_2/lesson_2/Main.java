564e5374be14b6f522d3fb4376d1806b3abcc0d0
Andrew Yushkevich <yushkevichandrew@gmail.com>
2019-03-03 11:35:09 +0300

    something

--- src/yushkevich/java_2/lesson_2/Main.java
+++ src/yushkevich/java_2/lesson_2/Main.java
@@ -2,6 +2,6 @@ package yushkevich.java_2.lesson_2;
 
 public class Main {
     public static void main(String[] args) {
-        ArrStr arrStr = new ArrStr(new ArrStr[5][5]);
+        Сalculations c = new Сalculations();
     }
 }
