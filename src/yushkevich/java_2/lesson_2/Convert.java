c84f23295930b4841c772eaf3c6de4bb69c762ec
Andrew Yushkevich <yushkevichandrew@gmail.com>
2019-03-03 16:17:37 +0300

    bad descision

--- src/yushkevich/java_2/lesson_2/Convert.java
+++ src/yushkevich/java_2/lesson_2/Convert.java
@@ -1,16 +1,26 @@
 package yushkevich.java_2.lesson_2;
 
 public class Convert {
-    public void makeInt(StrArr s) {
-        if (s.ar.length != 4) {
-            throw new MyArraySizeException("StrArr must be 4x4");
-        }
+    public void makeInt(StrArr s){
+        int sum = 0;
         try {
-            for (String i[] : s.ar) {
-                for (String g[] : s.ar) {
+            for (int i = 0; i < s.ar.length; i++) {
+                if (s.ar.length != 2) {
+                    throw new MyArraySizeException("StrArr must be 4x4");
+                }
+                for (int g = 0; g < s.ar.length; g++) {
+                    if (s.ar.length != 2) {
+                        throw new MyArraySizeException("StrArr must be 4x4");
+                    }
+                    try {
+                        sum += Integer.parseInt(s.ar[i][g]);
+                    } catch (NumberFormatException e) {
+                        throw new MyArrayDataException(i, g);
+                    }
                 }
             }
-        } catch (MyArraySizeException e) {
+            System.out.println(sum);
+        } catch (MyArraySizeException | MyArrayDataException e) {
             System.out.println(e.getMessage());
         }
     }
--- src/yushkevich/java_2/lesson_2/Main.java
+++ src/yushkevich/java_2/lesson_2/Main.java
@@ -1,8 +1,8 @@
 package yushkevich.java_2.lesson_2;
 
 public class Main {
-    public static void main(String[] args) {
-        StrArr correct_arr = new StrArr(new String[][]{{"1", "2"},{"3", "4"}, {"2", "3"}, {"7","4"}});
+    public static void main(String[] args){
+        StrArr correct_arr = new StrArr(new String[][]{{"1", "2"},{"6", "4"}});
         new Convert().makeInt(correct_arr);
     }
 }
