6acaeb7010d7a91d62ee07e8b39287b8c4ae2e2e
Andrew Yushkevich <yushkevichandrew@gmail.com>
2019-03-05 22:23:26 +0300

    something

--- src/yushkevich/java_2/lesson_3/Main.java
+++ src/yushkevich/java_2/lesson_3/Main.java
@@ -1,4 +1,25 @@
 package yushkevich.java_2.lesson_3;
 
+import java.util.ArrayList;
+
 public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> rand = new ArrayList<>();
+        rand.add("Клара");
+        rand.add("У");
+        rand.add("Карл");
+        rand.add("Крала");
+        rand.add("Кораллы");
+        rand.add("Карл");
+        rand.add("У");
+        rand.add("Клара");
+        rand.add("Свистнул");
+        rand.add("Кларнет");
+        rand.add("Бенедикт");
+        rand.add("Чпоказавр");
+        rand.add("ЧопЧопберберч");
+
+        System.out.println(rand);
+
+    }
 }
