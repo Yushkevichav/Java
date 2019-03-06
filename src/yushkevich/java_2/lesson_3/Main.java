f7abd47fd328d83ba039d9b03eb23c4106ac8bc3
Andrew Yushkevich <yushkevichandrew@gmail.com>
2019-03-06 20:19:20 +0300

    not my decision

--- src/yushkevich/java_2/lesson_3/Main.java
+++ src/yushkevich/java_2/lesson_3/Main.java
@@ -13,17 +13,17 @@ public class Main {
         }
         System.out.println(word);
 
-//        Phonebook book = new Phonebook();
-//        book.addContact("Roma", "363487");
-//        book.addContact("Rita", "870977");
-//        book.addContact("Kola", "544456");
-//        book.addContact("Roma", "877765");
-//        book.addContact("Sena", "877737");
-//        book.addContact("Sena", "877733");
-//
-//        book.findAndPrint("Roma");
-//        book.findAndPrint("Sena");
-//        book.findAndPrint("Kola");
+        Phonebook book = new Phonebook();
+        book.addContact("Roma", "363487");
+        book.addContact("Rita", "870977");
+        book.addContact("Kola", "544456");
+        book.addContact("Roma", "877765");
+        book.addContact("Sena", "877737");
+        book.addContact("Sena", "877733");
+
+        book.findAndPrint("Roma");
+        book.findAndPrint("Sena");
+        book.findAndPrint("Kola");
 
     }
 }
