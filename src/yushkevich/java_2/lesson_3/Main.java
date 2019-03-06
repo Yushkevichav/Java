ab8c2cb2354a107b5014ea0c87376f150347e125
Andrew Yushkevich <yushkevichandrew@gmail.com>
2019-03-06 20:12:42 +0300

    something

--- src/yushkevich/java_2/lesson_3/Main.java
+++ src/yushkevich/java_2/lesson_3/Main.java
@@ -4,35 +4,26 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String[] name = {"Клара",
-                "У",
-                "Карл",
-                "Крала",
-                "Кларнет",
-                "Карл",
-                "У",
-                "Клара",
-                "Свистнул",
-                "Кораллы",
-                "Кларнет"};
+        String[] name = {"Клара", "У", "Карл", "Крала", "Кларнет", "Карл", "У", "Клара", "Свистнул", "Кораллы", "Кларнет"};
 
         HashMap<String, Integer> word = new HashMap<>();
-        for (String x : name) {
-            word.put(x, word.getOrDefault(x,0)+1);
+
+        for (String some : name) {
+            word.put(some, word.getOrDefault(some, 0) + 1);
         }
         System.out.println(word);
 
-        Phonebook book = new Phonebook();
-        book.addContact("Roma", "363487");
-        book.addContact("Rita", "870977");
-        book.addContact("Kola", "544456");
-        book.addContact("Roma", "877765");
-        book.addContact("Sena", "877737");
-        book.addContact("Sena", "877733");
-
-        book.findAndPrint("Roma");
-        book.findAndPrint("Sena");
-        book.findAndPrint("Kola");
+//        Phonebook book = new Phonebook();
+//        book.addContact("Roma", "363487");
+//        book.addContact("Rita", "870977");
+//        book.addContact("Kola", "544456");
+//        book.addContact("Roma", "877765");
+//        book.addContact("Sena", "877737");
+//        book.addContact("Sena", "877733");
+//
+//        book.findAndPrint("Roma");
+//        book.findAndPrint("Sena");
+//        book.findAndPrint("Kola");
 
     }
 }
