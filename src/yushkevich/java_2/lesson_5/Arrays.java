fddf0a405bdc52439a2569fa05df3f8483c0c7dd
Andrew Yushkevich <yushkevichandrew@gmail.com>
2019-03-15 10:34:28 +0300

    refactor

--- src/yushkevich/java_2/lesson_5/Arrays.java
+++ src/yushkevich/java_2/lesson_5/Arrays.java
@@ -5,8 +5,9 @@ public class Arrays {
     private final int HALF = SIZE / 2;
 
     public float[] calculate(float[] arr) {
-        for (int i = 0; i < arr.length; i++)
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
         return arr;
     }
 
